a81dc4cbcfc861fa49094a4458ac3bc50dd9533e
M2LTN <aaron.malton@voco.ee>
2025-05-27 21:54:27 +0300

    Create users table and relation to products table data

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const sequelize = require('./util/db');
+
+const models = require('./models/index');
+sequelize.models = models;
+
+app.use((req, res, next) => {
+    models.User.findByPk(1)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
+  
 
 const productAdminRoutes = require('./routes/admin/products');
 app.use('/admin', productAdminRoutes);
@@ -11,17 +27,23 @@ app.use('/admin', productAdminRoutes);
 const productRoutes = require('./routes/products')
 app.use(productRoutes)
 
-const sequelize = require('./util/db');
-
-const models = require('./models/index');
-sequelize.models = models;
 
 sequelize
-  .sync()
+  .sync({force: true})
   .then(() => {
-    console.log('Tabelid on loodud');
-    app.listen(3003);
+    return models.User.findByPk(1);
   })
+  .then(user => {
+    if (!user) {
+      return models.User.create({ name: 'user', email: 'user@local.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    console.log(user);
+    app.listen(3002);
+  })
+  
   .catch((error) => {
     console.log(error);
   });
@@ -31,4 +53,4 @@ app.get('/', (req, res) => {
   res.json({ message: 'web shop app' });
 });
 
-app.listen(3002);
+
